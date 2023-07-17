3ffa1304b89913120ef1a8920fc5907167a68374
Teslanot <daniilaleks8@gmail.com>
2023-07-17 09:21:11 +0300

    Initial commit

---
+++ lesson_4/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse# класс для отправки ответа
+
+# функция-представление
+def lesson(request):
+    return HttpResponse("ДЗ")
