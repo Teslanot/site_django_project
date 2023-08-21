4bb01221c20b4e314097584db511350e7b63d3dd
Teslanot <daniilaleks8@gmail.com>
2023-08-21 19:13:57 +0300

    9.0

--- add/forms.py
+++ add/forms.py
@@ -1,26 +1,43 @@
 from django import forms
+from django.forms import ModelForm
 from django.core.validators import RegexValidator
+from add.models import Advertisement
 
 
-class AdverisementForm(forms.Form):
-    # class="row mb-3 offset-sm-4"
-    title       = forms.CharField(max_length=100, widget=forms.TextInput(
-        {"class": "form-control-lg"}
-    ), validators=[RegexValidator('[?+-/%]', inverse_match=True)]) # validators работает
-    descriptions = forms.CharField(widget=forms.Textarea(
-        {"class": "form-control-lg"}
-    ))
-    prices       = forms.DecimalField(widget=forms.NumberInput(
-        {"class": "form-control-lg"}
-    ))
-    auction     = forms.BooleanField(required=False, widget=forms.CheckboxInput(
-        {"class": "form-check-input"}
-    )) # поле необязательное
-    image       = forms.ImageField(widget=forms.FileInput(
-        {"class": "form-control-lg"}
-    ))
+# ModelForm----------------
+class Advforma(ModelForm): 
+    class Meta:
+        model = Advertisement
+        fields = ['title', 'descriptions', 'prices', 'auction', 'image']
+
+
+
+form = Advforma()
+
+# AdverisementForm
+# END -----------
+
+# forms.Form---------------
+# class AdverisementForm(forms.Form):
+#     # class="row mb-3 offset-sm-4"
+#     title       = forms.CharField(max_length=100, widget=forms.TextInput(
+#         {"class": "form-control-lg"}
+#     ), validators=[RegexValidator('[?+-/%]', inverse_match=True)]) # validators работает
+#     descriptions = forms.CharField(widget=forms.Textarea(
+#         {"class": "form-control-lg"}
+#     ))
+#     prices       = forms.DecimalField(widget=forms.NumberInput(
+#         {"class": "form-control-lg"}
+#     ))
+#     auction     = forms.BooleanField(required=False, widget=forms.CheckboxInput(
+#         {"class": "form-check-input"}
+#     )) # поле необязательное
+#     image       = forms.ImageField(widget=forms.FileInput(
+#         {"class": "form-control-lg"}
+#     ))
 
     # title.widget.attrs.update({'class': 'special'})
+
 
 
     # def clean_recipients(self):
@@ -31,7 +48,7 @@ class AdverisementForm(forms.Form):
     #     # Always return a value to use as the new cleaned data, even if
     #     # this method didn't change it.
     #     return data
-
+# END-----------------
 
 
 
--- add/views.py
+++ add/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,redirect # для того чтобы отдавать html
 from django.urls import reverse #получение ссылки по названию в urls
 from .models import Advertisement
-from .forms import AdverisementForm
+# from .forms import AdverisementForm
+from .forms import Advforma
 from django.core.handlers.wsgi import WSGIRequest
 
 
@@ -17,9 +18,7 @@ def home(request):
     return render(request, 'index.html', context)
 
 
-
-
-
+# AdvForma
 def post_adv(request: WSGIRequest):
     
     print('request.GET',request.GET)
@@ -28,7 +27,7 @@ def post_adv(request: WSGIRequest):
     print('request.user',request.user)
 
     if request.method == "POST":
-        form = AdverisementForm(request.POST, request.FILES) # передаю данные с запроса на проверку 
+        form = Advforma(request.POST, request.FILES) # передаю данные с запроса на проверку 
         if form.is_valid(): # True/False  проверяю правильность
             # print(request.POST['title'])
             print(form.cleaned_data) # отдает словарь со всеми данными
@@ -44,7 +43,35 @@ def post_adv(request: WSGIRequest):
 
 
     else: # GET или другие
-        form = AdverisementForm() # пустая форма
+        form = Advforma()
+# ----------------------------------------------------------------
+
+
+# def post_adv(request: WSGIRequest):
+    
+#     print('request.GET',request.GET)
+#     print('request.POST',request.POST)
+#     print('request.FILES',request.FILES)
+#     print('request.user',request.user)
+
+#     if request.method == "POST":
+#         form = AdverisementForm(request.POST, request.FILES) # передаю данные с запроса на проверку 
+#         if form.is_valid(): # True/False  проверяю правильность
+#             # print(request.POST['title'])
+#             print(form.cleaned_data) # отдает словарь со всеми данными
+#             adv = Advertisement(**form.cleaned_data) # распаковка словаря
+#             adv.user = request.user # отдельно указал пользователя 
+#             adv.save()  # сохраняю запись
+#             return redirect(
+#                 reverse('home') # переадресация на главную страницу 
+#             )
+
+#         else: # если неправильно
+#             print(form.errors) # вывожу эту ошибку
+
+
+#     else: # GET или другие
+#         form = AdverisementForm() # пустая форма
 
 
 
